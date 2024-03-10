58ba4aec69b00aa7113b1eaad23d03c3be31c9ff
MARVIN <o.silverygold@gmail.com>
2024-03-10 21:47:24 +0300

    Update routes: remove missions path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Mission from './components/Mission';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
 
@@ -9,7 +8,6 @@ export function App() {
     <>
     <Routes>
       <Route path='/' element={<Home />}/>
-      <Route path='/missions' element={<Mission />}/>
       <Route path='*' element={<NotFound />}/>
     </Routes>
     </>
