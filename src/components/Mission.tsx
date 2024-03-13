66755b828a00669a992dc91e340734957938bcd1
MARVIN <o.silverygold@gmail.com>
2024-03-13 19:07:59 +0300

    Use custom hook in mission component

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -1,13 +1,16 @@
 import { useRef } from "react";
+import { useCounter } from "../hooks/useCounter";
 
 const Mission = ({ handleClick }: {
   handleClick: (id: string) => void;
 }) => {  
   const nameRef = useRef<HTMLInputElement>(null);
+  const { counter, increment } = useCounter({});
 
   return (
     <>
-      <h1>Mission here</h1>
+      <h1>Mission {counter} here</h1>
+      <button onClick={() => increment()}>Increment</button>
       <button onClick={() => handleClick(nameRef.current?.value as string)}>Hot</button>
       <input type="text" ref={nameRef} placeholder="Enter text"/>
     </>
